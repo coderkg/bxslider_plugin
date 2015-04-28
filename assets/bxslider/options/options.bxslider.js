82fffed04cb95f34acded5c9759511ce1b549017
Keith <coderkg@users.noreply.github.com>
2015-04-28 18:55:54 +0100

    initial commit

---
+++ assets/bxslider/options/options.bxslider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  //mode: 'fade',
+  captions: true,
+  auto: true,
+  autoControls: true,
+  minSlides: 2,
+  maxSlides: 2,
+  slideWidth: 360,
+  slideMargin: 10
+});
+});
---
+++ assets/bxslider/options/options.bxsliderx1.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.bxsliderx1').bxSlider({
+  //mode: 'fade',
+  captions: true,
+  auto: true,
+  //autoControls: true,
+  //minSlides: 2,
+  //maxSlides: 2,
+  //slideWidth: 360,
+  //slideMargin: 10,
+  infiniteLoop: false,
+  hideControlOnEnd: true
+});
+});
